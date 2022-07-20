5170fa56294670d74271cc0d21afe79c6a2f08b4
ClaireSubiali <claire-subiali@hotmail.fr>
2022-07-20 14:56:21 +0200

    Header integration with fontawesome

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,53 @@
 // import PropTypesLib from 'prop-types';
 import './style.scss';
 
+// import des éléments de FontAwesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlus, faUser, faNewspaper, faShop, faChess, faDiceD20, faShieldHalved, faCaretDown
+} from '@fortawesome/free-solid-svg-icons';
+
+import logo from '../../assets/img/logo-meeple.svg';
+
+// import logo from '/src/assets/img/logo-meeple.svg';
+
 // == Composant
 function Header() {
   return (
-    <div>Header</div>
+    <header>
+      <div id="head-logo">
+        <div id="left-header">
+          <a href="index.html">
+            <img src={logo} className="logotype" alt="Logo Meeple Bons PLans" />
+          </a>
+          <div className="header__title">MEEPLE BONS PLANS</div>
+        </div>
+        <div id="right-header">
+          <div className="search">
+            <input type="search" className="search-bar" name="q" placeholder="rechercher" />
+          </div>
+          <button type="button" id="add-deal"><FontAwesomeIcon icon={faPlus} /><span className="displaybutton"> Ajouter un bon plan</span></button>
+          <button type="button" id="login"><FontAwesomeIcon icon={faUser} /><span className="displaybutton"> Connexion</span></button>
+        </div>
+      </div>
+      <nav>
+        <div className="topnav" id="myTopnav">
+          <div className="dropdown">
+            <button type="button" className="dropbtn"><span>Bons Plans </span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </button>
+            <div className="dropdown-content animate">
+              <a href="#">Tous les bons plans</a>
+              <a href="#"><FontAwesomeIcon icon={faChess} /> Jeux de société</a>
+              <a href="#"><FontAwesomeIcon icon={faShieldHalved} /> Jeux de figurines</a>
+              <a href="#"><FontAwesomeIcon icon={faDiceD20} />  Jeux de rôle</a>
+            </div>
+          </div>
+          <a href="#"><FontAwesomeIcon icon={faNewspaper} /><span className="nav-link"> Actualité</span></a>
+          <a href="#"><FontAwesomeIcon icon={faShop} /><span className="nav-link"> Boutiques</span></a>
+        </div>
+      </nav>
+    </header>
   );
 }
 
