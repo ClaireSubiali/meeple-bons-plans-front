2944c3b4cb2b66f75e7cb375d50b39ef951645cb
JulienFnz <fernandezj@laposte.net>
2022-07-22 10:33:11 +0200

    create login form popup

--- integration/script.js
+++ integration/script.js
@@ -8,3 +8,12 @@ window.addEventListener("scroll", function (e) {
     let footer = document.querySelector("footer");
     footer.classNameList.toggle("scrollEffect", window.scrollY > 0);
   });
+
+
+  function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+  }
