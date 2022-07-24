ff81fe8884b82f25a38e80bb95b191a876797d1b
JulienFnz <fernandezj@laposte.net>
2022-07-24 18:27:26 +0200

    update register user page

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -12,14 +12,23 @@ function SignIn() {
   return (
     <div className="Parent">
       <div className="newUser">
-        <div className="title"><FontAwesomeIcon icon={faAddressCard} /> <h1 className="h1-inscription"> Créer un compte</h1></div>
-        <input className="input--inscription" placeholder="Choisir un pseudo" type="text" />
+        <div className="header-register">
+          <div className="title"><FontAwesomeIcon icon={faAddressCard} /> </div>
+          <div className="title-inscription"> Créer un compte</div>
+        </div>
+        <input className="input-register" placeholder="Choisir un pseudo" type="text" />
         <span className="input--span">Ce pseudo n'est pas disponible</span>
-        <input className="input--inscription" placeholder="Adresse Mail" type="text" />
+        <input className="input-register" placeholder="Adresse Mail" type="text" />
         <span className="input--span">L'adresse mail n'est pas valide</span>
-        <input className="input--inscription" placeholder="Mot de passe" type="text" />
-        <input className="input--inscription" placeholder="Confirmer le mot de passe" type="text" />
+        <input className="input-register" placeholder="Mot de passe" type="text" />
+        <input className="input-register" placeholder="Confirmer le mot de passe" type="text" />
         <span className="input--span">Le mot de passe n'est pas identique</span>
+        <select name="avatars" id="meeple-select">
+          <option value="">--Choisis une couleur de MEEPLE--</option>
+          <option value="rouge">Rouge</option>
+          <option value="vert">Vert</option>
+          <option value="blue">Blue</option>
+        </select>
         <button type="button" className="button--inscription">Je m'inscris</button>
       </div>
     </div>
