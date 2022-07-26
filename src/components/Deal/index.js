1301947c0995d09711cc6abf8be89d909edadc79
JulienFnz <fernandezj@laposte.net>
2022-07-26 22:59:25 +0200

    deal id page ok with responsive

--- src/components/Deal/index.js
+++ src/components/Deal/index.js
@@ -9,41 +9,82 @@ import flameIcon from '../../assets/img/logo-flamme.svg';
 import iceCubeIcon from '../../assets/img/logo-icecube.svg';
 import testImage from '../../assets/img/img-test.png';
 
+import avatar from '../../assets/img/m-orange.png';
+
 // == Composant
 function Deal() {
   return (
-    <div className="deal detail">
-      <div className="left-deal">
-        <img className="picture-deal" src={testImage} alt="Bon plan" />
-        <div className="vote">
-          <div className="icone-degree">
-            <img className="flamme" src={flameIcon} alt="Icone flamme" />
+    <div className="deal-detail-comments">
+      <div className="deal detail-card">
+        <div className="left-deal">
+          <img className="picture-deal" src={testImage} alt="Bon plan" />
+          <div className="vote">
+            <div className="icone-degree">
+              <img className="flamme" src={flameIcon} alt="Icone flamme" />
+            </div>
+            <div className="degree">25°</div>
+            <div className="icone-degree">
+              <img className="icecube" src={iceCubeIcon} alt="Icone glacon" />
+            </div>
           </div>
-          <div className="degree">25°</div>
-          <div className="icone-degree">
-            <img className="icecube" src={iceCubeIcon} alt="Icone glacon" />
+        </div>
+        <div className="right-deal">
+          <div className="header-deal">
+            <div className="title-deal">
+              <h2>Dixit</h2>
+              <h6>Bon plan jeux de société<span className="shop"> | Philibert</span></h6>
+            </div>
+            <div className="deal-label">
+              <div className="discount">-20%</div>
+              <div className="price">20.90€</div>
+            </div>
+          </div>
+          <div className="main-deal">
+            <p className="deal-text">Le principe de Dixit est simple : les joueurs doivent deviner et faire deviner des cartes illustrées. À chaque tour, un joueur devient le conteur qui choisit une carte et la décrit avec une phase, un mot ou un son...</p>
+          </div>
+          <div className="footer-deal">
+            <p className="footer-dealp">
+              <button type="button" className="see-deal">Voir le bon plan</button>
+            </p>
+            <p className="footer-deal-time"><span className="clock"><FontAwesomeIcon icon={faClock} /></span> il y a 15 minutes</p>
           </div>
         </div>
       </div>
-      <div className="right-deal">
-        <div className="header-deal">
-          <div className="title-deal">
-            <h2>Dixit</h2>
-            <h6>Bon plan jeux de société<span className="shop"> | Philibert</span></h6>
-          </div>
-          <div className="deal-label">
-            <div className="discount">-20%</div>
-            <div className="price">20.90€</div>
-          </div>
+      <div className="deal-comments com-user">
+        <div className="avatar-user-com"><img src={avatar} alt="profil meeple" id="avatar" />
         </div>
-        <div className="main-deal">
-          <p className="deal-text">Le principe de Dixit est simple : les joueurs doivent deviner et faire deviner des cartes illustrées. À chaque tour, un joueur devient le conteur qui choisit une carte et la décrit avec une phase, un mot ou un son...</p>
+        <div className="comment-textarea">
+          <label className="deal-secondarytitle" htmlFor="message-comments">Commentaire</label>
+          <textarea
+            rows="6"
+            className="deal-form-message dealmsg"
+            placeholder="Ecrivez votre commentaire ici"
+          />
         </div>
-        <div className="footer-deal">
-          <p className="footer-dealp">
-            <button type="button" className="see-deal">Voir le bon plan</button>
-          </p>
-          <p className="footer-deal-time"><span className="clock"><FontAwesomeIcon icon={faClock} /></span> il y a 15 minutes</p>
+        <button type="button" className="deal-form-button">Envoyer</button>
+      </div>
+
+      <div className="deal-comments com-user">
+        <div className="avatar-user-com"><img src={avatar} alt="profil meeple" id="avatar" />
+        </div>
+        <div className="comment-textarea">
+          <p className="deal-secondarytitle">Michel</p>
+          <p className="deal-form-response">C'est un super deal !</p>
+        </div>
+      </div>
+
+      <div className="deal-comments com-user">
+        <div className="avatar-user-com"><img src={avatar} alt="profil meeple" id="avatar" />
+        </div>
+        <div className="comment-textarea">
+          <p className="deal-secondarytitle">Jacouille de Limoges</p>
+          <p className="deal-form-response">Merci pour le bon plan, j'ai craqué</p>
+        </div>
+      </div>
+
+      <div className="deal-comments endmsg">
+        <div className="comment-textarea">
+          <p className="deal-msg-end">FIN DES MESSAGES</p>
         </div>
       </div>
     </div>
