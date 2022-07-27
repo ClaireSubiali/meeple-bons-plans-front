1c8dfd2dfdee14bb1585c42621ef5570c4c91bf2
laurent <laurent.supplisson@gmail.com>
2022-07-27 14:11:15 +0200

    W-in-progress structure and css MVP

--- src/components/ShopList/index.js
+++ src/components/ShopList/index.js
@@ -3,12 +3,14 @@
 import "./style.scss";
 import magasinImg from './Bandeau.jpg';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faStore, faChrome } from "@fortawesome/free-solid-svg-icons";
+import { faStore, faGlobe } from "@fortawesome/free-solid-svg-icons";
 
 //on creer le composant
 function ShopList() {
   return (
     <div className="pseudo-body">
+  
+
       <article className="shop-card">
         <h1 className="title-shop">La boutique</h1>
         <div className="shop-card-contener">
@@ -27,17 +29,42 @@ function ShopList() {
               <FontAwesomeIcon className="icone-store" icon={faStore} />
               <span>12 rue de la boustifaille</span>
             </div>
-            <div>
+            <div className="contener-site">
+              <FontAwesomeIcon className="icone-store" icon={faGlobe} />
+              <span>www.ici.com</span>
+            </div>
+          </div>
+        </div>
+      </article>
+
+      
+      <article className="shop-card">
+        <h1 className="title-shop">La boutique</h1>
+        <div className="shop-card-contener">
+          <div className="card-shop-left">
+            <img className="img-shop" src={magasinImg} alt="Img de la boutique"></img>
+            <button className="button-shop-detail">Voir la boutique</button>
+          </div>
+          <div className="card-shop-right">
+            <p className="description-shop">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a
+              rutrum justo, non consequat quam. Aliquam nec quam et quam ornare
+              porttitor at non mauris. Interdum et malesuada fames ac ante ipsum
+              primis in faucibus.
+            </p>
+            <div className="contener-store">
               <FontAwesomeIcon className="icone-store" icon={faStore} />
+              <span>12 rue de la boustifaille</span>
+            </div>
+            <div className="contener-site">
+              <FontAwesomeIcon className="icone-store" icon={faGlobe} />
               <span>www.ici.com</span>
             </div>
           </div>
         </div>
       </article>
     </div>
-  ); /*1 magasin par ligne voir camille 
-  pas de aside 
-  10 resultats par page */
+  );
 }
 
 export default ShopList;
