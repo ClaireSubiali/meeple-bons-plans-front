d265752bb8e26c8d22bc11ca39aeddd1b6149a5d
JulienFnz <fernandezj@laposte.net>
2022-08-01 11:24:45 +0200

    404 error

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -2,10 +2,17 @@
 // import PropTypesLib from 'prop-types';
 import './style.scss';
 
+// import image error 404
+import err404 from '../../assets/img/404-img.png';
+
 // == Composant
 function Error() {
   return (
-    <div>Page 404</div>
+    <div className="error">
+      <p className="errormsg">Oops! Tu vas devoir passer ton tour…</p>
+
+      <img src={err404} alt="erreur 404" id="404-error" />
+    </div>
   );
 }
 
