29bf2d1a81878bea2c0433ea6137445b2c438212
JulienFnz <fernandezj@laposte.net>
2022-08-02 21:15:41 +0200

    css reponsive for popup search game

--- src/components/AddDeal/SearchGamePopUp.js
+++ src/components/AddDeal/SearchGamePopUp.js
@@ -14,15 +14,15 @@ function SearchGamePopUp({
 }) {
   return (
     <div className="login-popup">
-      <div className="form-popup">
+      <div className="form-popup searchgamepopup">
         <div action="#" className="form-container">
           <div id="title_login"><FontAwesomeIcon icon={faMagnifyingGlass} />Résultats de votre recherche<span><button aria-label="Fermer" type="button" className="cancel" onClick={ToggleSearchGame}><FontAwesomeIcon icon={faXmark} /></button></span>
           </div>
           <div className="results_searchgame">
             <ul>
-              <li>Jeu</li>
-              <li>Jeu</li>
-              <li>Jeu</li>
+              <li><img src="https://www.myludo.fr/img/jeux/1640829454/300/by/50473.png"/><span>Dixit</span></li>
+              <li><img src="https://www.myludo.fr/img/jeux/1640829454/300/by/50473.png"/><span>Ici le nom du second jeu</span></li>
+              <li><img src="https://www.myludo.fr/img/jeux/1640829454/300/by/50473.png"/><span>Ici le nom du 3e jeu</span></li>
             </ul>
           </div>
         </div>
